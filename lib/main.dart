3df9737bee855f9b0ebd5fb6b00d353ef21bd6b8
Mateo Ortiz <bptecgaming@outlook.com>
2024-06-02 14:21:51 -0500

    Test 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wallpaper App',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
