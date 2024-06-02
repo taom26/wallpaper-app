3c95cf532e38c1368a3bd27a46ddf0fbf8d5f653
Mateo Ortiz <bptecgaming@outlook.com>
2024-06-02 16:45:15 -0500

    Paginas varias

--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -1,17 +1,158 @@
 import 'package:flutter/material.dart';
+import 'package:wallpaper_app/views/screens/color_picker.dart';
+import 'package:wallpaper_app/views/screens/sprite_walking.dart';
+import 'package:wallpaper_app/views/screens/settings.dart';
 import 'package:wallpaper_app/views/widgets/CustomAppBar.dart';
 
-class HomeScreen extends StatelessWidget {
+class ExampleDestination {
+  const ExampleDestination(this.label, this.icon, this.selectedIcon);
+
+  final String label;
+  final Widget icon;
+  final Widget selectedIcon;
+}
+
+const List<ExampleDestination> destinations = <ExampleDestination>[
+  ExampleDestination(
+      'Colores', Icon(Icons.palette_outlined), Icon(Icons.palette)),
+  ExampleDestination(
+      'PJ Bounce', Icon(Icons.android_outlined), Icon(Icons.android)),
+  ExampleDestination(
+      'Wallpapers', Icon(Icons.wallpaper_outlined), Icon(Icons.wallpaper)),
+];
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  int screenIndex = 0;
+  late bool showNavigationDrawer;
+
+  void handleScreenChanged(int selectedScreen) {
+    setState(() {
+      screenIndex = selectedScreen;
+    });
+  }
+
+  // Define the different pages
+  final List<Widget> pages = [
+    const HomePage(),
+    const TrendingPage(),
+    const SettingsPage(),
+  ];
+
+  Widget buildBottomBarScaffold() {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 92, 92, 92),
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: const Color.fromARGB(255, 61, 61, 61),
-          title: const CustomAppBar(),
-        ));
+      appBar: AppBar(
+        title: const CustomAppBar(),
+        backgroundColor: const Color.fromARGB(255, 61, 61, 61),
+      ),
+      body: pages[screenIndex], // Display the selected page
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: screenIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            screenIndex = index;
+          });
+        },
+        destinations: destinations.map((ExampleDestination destination) {
+          return NavigationDestination(
+            label: destination.label,
+            icon: destination.icon,
+            selectedIcon: destination.selectedIcon,
+            tooltip: destination.label,
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget buildDrawerScaffold(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: const CustomAppBar(),
+        backgroundColor: const Color.fromARGB(255, 61, 61, 61),
+      ),
+      body: SafeArea(
+        bottom: false,
+        top: false,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: NavigationRail(
+                minWidth: 50,
+                destinations: destinations.map(
+                  (ExampleDestination destination) {
+                    return NavigationRailDestination(
+                      label: Text(destination.label),
+                      icon: destination.icon,
+                      selectedIcon: destination.selectedIcon,
+                    );
+                  },
+                ).toList(),
+                selectedIndex: screenIndex,
+                useIndicator: true,
+                onDestinationSelected: (int index) {
+                  setState(() {
+                    screenIndex = index;
+                  });
+                },
+              ),
+            ),
+            const VerticalDivider(thickness: 1, width: 1),
+            Expanded(
+              child: pages[screenIndex], // Display the selected page
+            ),
+          ],
+        ),
+      ),
+      endDrawer: NavigationDrawer(
+        onDestinationSelected: handleScreenChanged,
+        selectedIndex: screenIndex,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(28, 16, 16, 10),
+            child: Text(
+              'Header',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ),
+          ...destinations.map(
+            (ExampleDestination destination) {
+              return NavigationDrawerDestination(
+                label: Text(destination.label),
+                icon: destination.icon,
+                selectedIcon: destination.selectedIcon,
+              );
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+            child: Divider(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    showNavigationDrawer = MediaQuery.of(context).size.width >= 450;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return showNavigationDrawer
+        ? buildDrawerScaffold(context)
+        : buildBottomBarScaffold();
   }
 }
